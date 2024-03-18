1d23699c9cc0056df45726a96f218f9f3473d12f
artemLink <45370882+artemLink@users.noreply.github.com>
2024-03-18 18:54:12 +0200

    add hw

---
+++ part2/project_1/decorators.py
@@ -0,0 +1,23 @@
+def input_error(func):
+    def wrapper(*args):
+        try:
+
+            return func(*args)
+        except IndexError as index_ex_message:
+            return index_ex_message
+        except ValueError as val_ex_message:
+            return val_ex_message
+        except KeyError as key_ex_message:
+            return key_ex_message
+        except TypeError as type_ex_message:
+            return type_ex_message
+        except AttributeError as atr_ex_message:
+            return atr_ex_message
+        except ZeroDivisionError as zero_div_ex_message:
+            return zero_div_ex_message
+        except FileNotFoundError as file_not_found_ex_message:
+            return file_not_found_ex_message
+        except PermissionError as permission_ex_message:
+            return permission_ex_message
+
+    return wrapper
